d39386cc723eb57b599c67e6b77299e0b9e268af
Ram Krishna Singh <rkscodes@gmail.com>
2025-02-22 21:03:20 +0530

    Add crossbeam and num_cpus for multithreading support in rendering

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ use std::env;
 /// Heart of MandelBrot
 /// Trick 1: Check till max number no of times as mentioned by limit
 /// Trick 2: Known if dis is >4 it will run to infinity eventually so breakout at that point
-/// TODO: Figure out way to do multiprocessing on diff cores and eventually limit could go to ifinity in reasonable amt of time.
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
@@ -149,7 +148,32 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    // render(&mut pixels, bounds, upper_left, lower_right);
+
+    //Multithreading (spare 2 cores for user)
+    let mut threads = num_cpus::get() - 2;
+    if threads <= 0 {
+        threads = 1
+    }
+
+    let rows_per_band = bounds.1 / threads + 1;
+
+    let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+    crossbeam::scope(|spawner| {
+        for (i, band) in bands.into_iter().enumerate() {
+            let top = rows_per_band * i;
+            let height = band.len() / bounds.0;
+            let band_bounds = (bounds.0, height);
+            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_lower_right =
+                pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+            spawner.spawn(move |_| {
+                render(band, band_bounds, band_upper_left, band_lower_right);
+            });
+        }
+    })
+    .unwrap();
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
